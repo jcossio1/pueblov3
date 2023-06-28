7e0691b556fd615f5b80f6d6723725523dcbb70d
Luis Torres <luistorresco@gmail.com>
2023-06-27 20:42:36 -0500

    se crea proyecto v2

---
+++ app/src/main/java/com/example/pueblov3/Home.java
@@ -0,0 +1,37 @@
+package com.example.pueblov3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Home extends AppCompatActivity {
+
+    //zona de los atributos
+
+    Button botonHoteles;
+    Button botonRestaurantes;
+    Button botonTurismo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        botonHoteles = findViewById(R.id.botonhoteles); //buscar id
+        botonRestaurantes = findViewById(R.id.botonrestaurante);
+        botonTurismo = findViewById(R.id.botonturismo);
+
+        //Detectando eventos
+        botonHoteles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(Home.this, "Estamos trabajando en ello", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
